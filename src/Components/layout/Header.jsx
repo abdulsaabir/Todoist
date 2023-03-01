147d9525c6374a49df78bf038a9e8e1bfdcba4ac
Eng-Abdulsaabir-Haaji <saabirhaaji10@gmail.com>
2023-03-01 10:25:20 +0300

    building the header

--- src/Components/layout/Header.jsx
+++ src/Components/layout/Header.jsx
@@ -1,5 +1,24 @@
+import React from "react";
+import logo from "../../assets/logo.png";
+import { FaPizzaSlice } from "react-icons/fa";
 const Header = () => {
-  return <div className="header" data-testid="header"></div>;
+  return (
+    <div className="header" data-testid="header">
+      <nav>
+        <div className="logo">
+          <img src={logo} alt="todo" />
+        </div>
+        <div className="settings">
+          <ul>
+            <li>+</li>
+            <li>
+              <FaPizzaSlice />
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
 };
 
 export default Header;
